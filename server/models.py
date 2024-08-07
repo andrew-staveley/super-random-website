9d8951580b07c9d2b8ea554023ca54393a7a8d99
Andrew Staveley <125606667+andrew-staveley@users.noreply.github.com>
2024-08-07 14:47:28 -0400

    Created models for user and posts, created relationship between the two

---
+++ server/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.exc import IntegrityError
+from config import db, bcrypt
+import datetime
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-posts.user', '-_password_hash')
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    name = db.Column(db.String)
+    bio = db.Column(db.String)
+    posts = db.relationship('Post', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+class Post(db.Model, SerializerMixin):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable = False)
+    timestamp = db.Column(datetime.db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
