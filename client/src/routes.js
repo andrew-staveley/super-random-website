07d49a9b6123d638ffdcc70490b29cefe91e86a9
Andrew Staveley <125606667+andrew-staveley@users.noreply.github.com>
2024-08-10 13:46:18 -0400

    Added Routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,11 @@
 import Home from "./pages/Home.js"
 import ErrorPage from "./pages/ErrorPage.js"
+import Login from "./pages/Login.js"
+import Account from "./pages/Account.js"
+import SignUp from "./pages/SignUp.js"
+import Feed from "./pages/Feed.js"
+import Post from "./pages/Post.js"
+import Profile from "./pages/Profile.js"
 
 const routes = [
     {
@@ -7,5 +13,35 @@ const routes = [
         element: <Home />,
         errorElement: <ErrorPage />
     },
+    {
+        path: "/login",
+        element: <Login />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/account",
+        element: <Account />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/signup",
+        element: <SignUp />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/feed",
+        element: <Feed />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/post/:id",
+        element: <Post />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/profile/:id",
+        element: <Profile />,
+        errorElement: <ErrorPage /> 
+    }
     ];
 export default routes;
