f80d34bb938922fcb0d52b1dd89547f5348d0784
Andrew Staveley <125606667+andrew-staveley@users.noreply.github.com>
2024-08-11 01:10:49 -0400

    Made small changes to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
+import "../styles/TheStyleFile.css"
 
 function Home() {
     return (
         <>
             <header>
+                <div class="navbarMimic">
+                </div>
             </header>
             <main>
                 <h1>Welcome to Twitter 2.0</h1>
                 <h3>Very clearly a work in progress lol.</h3>
                 <h4>Welcome to the party Loser</h4>
-                <Link to={"/login"}>Enter Here</Link>
+                <button class="button"><Link to={"/login"} >Enter Here</Link></button>
             </main>
         </>
     )
